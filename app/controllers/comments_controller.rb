ea9bfa253a8a3c2a53b0a0f6c0926a16b791ca94
daliayan <daliayan5@gmail.com>
2021-06-26 20:01:07 -0400

    Trouble with Comment being posted/created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    #before_action :require_login
 
     def index
     end
@@ -7,9 +8,24 @@ class CommentsController < ApplicationController
     end
 
     def new
+        require_login
+        @comment = Comment.new(house_id: params[:house_id])
+        # if params[:house_id] && @house = House.find_by_id(params[:house_id])
+        #     @comment = @house.comments.build
+        # else
+        #     @comment = Comment.new
+        # end
     end
 
     def create
+        @comment = Comment.new(comment_params)
+        @comment.house_id = params[:house_id]
+        #current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to comment_path(@comment)
+        else
+            render :new
+        end
     end
 
     def edit
@@ -21,6 +37,7 @@ class CommentsController < ApplicationController
     private
 
     def comment_params
+        params.require(:comment).permit(:content, :house_id)
     end
 
 end
