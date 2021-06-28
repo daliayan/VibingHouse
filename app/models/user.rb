d33057ffebee4dee50b64fc2c946191db62c5e45
daliayan <daliayan5@gmail.com>
2021-06-28 12:46:05 -0400

    Working on omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
     # has_many :rooms, through: :houses
     validates :username, presence: true
     validates_uniqueness_of :username
+
+    def self.from_omniauth(response)
+        User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
+            u.username = response[:info][:name]
+            u.password = SecureRandom.hex(15)
+        end
+    end
 end
