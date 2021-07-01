db1cf4473587f033a6ca542de2c9b652462c492e
daliayan <daliayan5@gmail.com>
2021-07-01 16:14:01 -0400

    Going to play with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,6 @@ class SessionsController < ApplicationController
             u.password = SecureRandom.hex(15)
         end
 
-        # user = User.from_omniauth(request.env['omniauth.auth'])
         if user.valid?
             session[:user_id] = user.id
             redirect_to user_path(user)
