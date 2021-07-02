b6aba2f60d590a05a2c6356bec62099683c0af0c
daliayan <daliayan5@gmail.com>
2021-07-02 13:35:35 -0400

    Found an error - will work through

--- app/models/house.rb
+++ app/models/house.rb
@@ -2,8 +2,6 @@ class House < ApplicationRecord
     belongs_to :user
     has_many :house_rooms
     has_many :rooms, through: :house_rooms
-    has_many :comments
-    has_many :users, through: :comments
 
     validates :price, :hex, :size, presence: true
     validates :name, presence: true, uniqueness: true
--- app/models/room.rb
+++ app/models/room.rb
@@ -4,15 +4,5 @@ class Room < ApplicationRecord
     has_many :users, through: :houses  
 
     validates :name, presence: true
-    
-    # validate :too_many_rooms
-
-
-    # def too_many_rooms
-    #     my_house = self.user.rooms
-    #     if my_house.size > 10
-    #         errors.add(:room_id, "There are too many rooms.")
-    #     end
-    # end
 
 end
